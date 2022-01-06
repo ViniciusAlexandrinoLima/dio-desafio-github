303e01194308ac691c9623e277f1d8dfe65884d9
Vinicius <viniciusalexandrino@hotmail.com>
2022-01-06 13:29:46 -0330

    Desafio Sintaxe e Operadores

---
+++ Desafio Sintaxe e Operadores/comparandoNumeros.js
@@ -0,0 +1,26 @@
+function comparandoNumeros(numero1, numero2) {
+    
+    if(!numero1 && !numero2) {return `Preencha os numeros!`};
+    
+    let soma = numero1 + numero2;
+    if(numero1 !== numero2) {
+        console.log(`Os numeros ${numero1} e ${numero2} não são iguais.`);
+        if(soma > 10) {
+            console.log(`Sua soma é: ${soma} que é maior que 10.`);
+        }else if(soma > 20) {
+            console.log(`Sua soma é: ${soma} que é maior que 20.`)
+        } 
+    }
+
+
+    if(numero1 === numero2) {
+        console.log(`Os numeros ${numero1} e ${numero2} são iguais.`);
+    }
+    if(soma < 10) {
+        console.log(`Sua soma é: ${soma} que é menor que 10.`);
+    } else if(soma < 20) {
+        console.log(`Sua soma é: ${soma} que é menor que 20.`)
+    } 
+}
+
+comparandoNumeros(1, 2);
